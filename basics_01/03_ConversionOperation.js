0fa1c69c790fdc491771822824efd87b50cecf93
trinathbsharma16 <153951729+trinathbsharma16@users.noreply.github.com>
2024-06-26 20:15:04 +0000

    Conversion_Operation

--- basics_01/03_ConversionOperation.js
+++ basics_01/03_ConversionOperation.js
@@ -20,3 +20,23 @@ console.log(booleanlogin);
 ""=> False
 "Trinath"=> True
 */
+                        // Operations
+let value=33
+let negValue= -value
+console.log(negValue);
+
+let str1="Hello"
+let str2=" Trinath B Sharma"
+let str3=str1+str2
+console.log(str3);
+console.log("1"+3)
+console.log(1+"3")
+console.log("1"+"3")
+console.log("1"+2+3)
+console.log(1+2+"3")
+
+console.log(+true);   //Conversion
+
+let gamecounter=100
+gamecounter++
+console.log(gamecounter);
