af8cba19ea97a28c6e64df3946b1d779840ed4a6
trinathbsharma16 <153951729+trinathbsharma16@users.noreply.github.com>
2024-06-26 09:22:59 +0000

    conversion_of_Data_Types

---
+++ basics_01/03_ConversionOperation.js
@@ -0,0 +1,22 @@
+let score="33aaa"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let ValueinNumber=Number(score);
+console.log(typeof ValueinNumber);
+console.log(ValueinNumber)
+/*
+33=>33
+Trinath=> NAN (Not a NUmber)
+true=>1   false=>0
+
+*/
+let islogin=1
+let booleanlogin=Boolean(islogin)
+console.log(booleanlogin);
+/*
+1=> True 0=>False
+""=> False
+"Trinath"=> True
+*/
