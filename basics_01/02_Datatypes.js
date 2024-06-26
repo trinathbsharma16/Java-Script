c64d663f5056d59ed30b9110aaa5d7387ac7b557
trinathbsharma16 <153951729+trinathbsharma16@users.noreply.github.com>
2024-06-26 08:24:56 +0000

    datatype

---
+++ basics_01/02_Datatypes.js
@@ -0,0 +1,11 @@
+"use strict"//Treats all JS code as newer version
+//alert(3+3)// Will give error as we are using node js not browser
+
+//Always the code readibility should be your priority 
+let name="Trinath B Sharma"//String
+let age=20//Int
+let isLoggedIn=true//Boolean
+//NULL=>Standalone Value   Object type 
+//Undefined=>No Value is assigned     Udefineed type
+//Stmbol=>Unique
+console.log(typeof null);
