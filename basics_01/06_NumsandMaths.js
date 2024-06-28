1e08a04b2689354aa07abdd918deac3fe202ae5f
trinathbsharma16 <153951729+trinathbsharma16@users.noreply.github.com>
2024-06-28 09:47:31 +0000

    Nums

---
+++ basics_01/06_NumsandMaths.js
@@ -0,0 +1,18 @@
+const score=400
+console.log(score);
+
+const balance=new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));// Returns a number 
+//console.log(typeof balance);
+//console.log(balance.toFixed(2));
+
+const othernumber=134.89
+console.log(othernumber.toPrecision(4)); //Returns string type
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-In'));
+
