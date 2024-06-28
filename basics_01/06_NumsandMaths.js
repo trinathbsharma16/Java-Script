e2140b8d7641f8e47568db24f30d60799e715130
trinathbsharma16 <153951729+trinathbsharma16@users.noreply.github.com>
2024-06-28 10:52:53 +0000

    Maths ans Num

--- basics_01/06_NumsandMaths.js
+++ basics_01/06_NumsandMaths.js
@@ -16,3 +16,22 @@ console.log(othernumber.toPrecision(4)); //Returns string type
 const hundreds=1000000
 console.log(hundreds.toLocaleString('en-In'));
 
+// ++++++++++++++++++++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.4));
+console.log(Math.floor(4.9));
+console.log(Math.min(54,98,110,15128));
+console.log(Math.max(54,98,110,15128));
+
+console.log(Math.random());// Will Always give value between 0 and 1
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
