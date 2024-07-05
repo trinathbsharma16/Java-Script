8e6ef5997d0a6b0ca4c9dfd0697631e86363e025
Trinath B Sharma <153951729+trinathbsharma16@users.noreply.github.com>
2024-07-05 17:55:46 +0000

    Truthy and false values

---
+++ 04_controlflow/truth_false.js
@@ -0,0 +1,11 @@
+//Falsy Value false ,0,-0,Big INT 0n,"",null ,undefined,NAN
+//Truth Value true,"0","false"or'false," ",[],{},function(){}
+const useremail=[]
+if(useremail.length===0){
+    console.log("The array is empty");
+}
+
+const objectis={}
+if(Object.keys(objectis).length===0){
+    console.log("Object is Empty");
+}
