273f416e3950c5fb90d1e18db4bd953aa4230991
Trinath B Sharma <153951729+trinathbsharma16@users.noreply.github.com>
2024-07-05 18:43:19 +0000

    Nullish Coalescing Operator

--- 04_controlflow/truth_false.js
+++ 04_controlflow/truth_false.js
@@ -9,3 +9,17 @@ const objectis={}
 if(Object.keys(objectis).length===0){
     console.log("Object is Empty");
 }
+
+//NUllish Coalescing Operator ?? works on null and undefined 
+
+val1=5?? null//Gives vale 5
+val2=undefined??10 //gives value 10
+val3=null??10??15// will return the first vlue not null or undefined
+console.log(val1);
+console.log(val2);
+console.log(val3);
+
+//Terniary operator 
+//condition ? true:false
+const iceteaprice=100
+iceteaprice<=80 ?console.log("less than 80"):console.log("more than 80");
