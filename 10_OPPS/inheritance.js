9ba6df3a40dddc8d05ca1e5e85fef48c5a9d8b5e
Trinath B Sharma <153951729+trinathbsharma16@users.noreply.github.com>
2024-08-25 09:09:01 +0000

    inheritance

--- 10_OPPS/inheritance.js
+++ 10_OPPS/inheritance.js
@@ -8,12 +8,25 @@ class user{
         
     }
 }
-class teacher extends User{
+class teacher extends user{
     constructor(username,email,password){
-        super(username)
+        super(username) // extends and super are the keyword which u need to add to  get the properties of the parent 
         this.email=email
         this.username=username
         this.password=password
         
     }
+    addcourse(){
+        console.log(`New course was added by${this.username}`);
+        
+    }
  }
+
+const newcourse=new teacher(" physics",'trinath@1234',"125")
+newcourse.addcourse()
+
+const newcourse2=new user("Trinath")
+newcourse2.logMe()
+
+console.log(newcourse instanceof teacher);
+console.log(newcourse instanceof user);
